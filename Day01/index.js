f8b1a7cd45d21eb2c98edde99d61ed7e9cab154d
Rampravesh <rampraveshkr4545@gmail.com>
2026-02-13 17:31:37 +0530

    i don't know

--- Day01/index.js
+++ Day01/index.js
@@ -23,3 +23,13 @@
 //     printNum(num);
 //   }
 // });
+
+console.time();
+const num = 100000000;
+let sum = 0;
+
+for (let i = 1; i <= num; i++) {
+  sum += i;
+}
+console.log(sum);
+console.timeEnd();
